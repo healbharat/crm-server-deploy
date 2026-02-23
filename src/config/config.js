e050c693fdedf137e613c29ec7473dc95c73c30a
Om Sonawane <healbharatofficial@gmail.com>
2026-02-23 16:26:59 +0530

    final mongoose 5 connection options fix

--- src/config/config.js
+++ src/config/config.js
@@ -51,6 +51,8 @@ module.exports = {
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
       autoIndex: true,
     },
   },
